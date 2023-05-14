9d160f3d724cc739c5aca46af02a428487487b39
lxthhhlxt <1205624872@qq.com>
2023-05-14 21:25:07 +0800

    ENH: 学习定时器、中断，并使用led制作demo
    details: as above

--- zero_program/Core/Src/main.c
+++ zero_program/Core/Src/main.c
@@ -19,6 +19,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "tim.h"
 #include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
@@ -43,7 +44,7 @@
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
-
+volatile int a = 0;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -64,7 +65,7 @@ void SystemClock_Config(void);
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-
+  
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -85,29 +86,30 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_TIM1_Init();
   /* USER CODE BEGIN 2 */
-
+  HAL_TIM_Base_Start_IT(&htim1);
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-    for(int r = 0; r < 2; r++)
-    {
-      for(int g = 0; g < 2; g++)
-      {
-        for(int b = 0; b < 2; b++)
-        {
-          HAL_GPIO_TogglePin(RGB_B_GPIO_Port, RGB_B_Pin);
-          HAL_Delay(200);
-        }
-        HAL_GPIO_TogglePin(RGB_G_GPIO_Port, RGB_G_Pin);
-        HAL_Delay(200);
-      }     
-      HAL_GPIO_TogglePin(RGB_R_GPIO_Port, RGB_R_Pin);
-      HAL_Delay(200);
-    }
+//    for(int r = 0; r < 2; r++)
+//    {
+//      for(int g = 0; g < 2; g++)
+//      {
+//        for(int b = 0; b < 2; b++)
+//        {
+//          HAL_GPIO_TogglePin(RGB_B_GPIO_Port, RGB_B_Pin);
+//          HAL_Delay(200);
+//        }
+//        HAL_GPIO_TogglePin(RGB_G_GPIO_Port, RGB_G_Pin);
+//        HAL_Delay(200);
+//      }     
+//      HAL_GPIO_TogglePin(RGB_R_GPIO_Port, RGB_R_Pin);
+//      HAL_Delay(200);
+//    }
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -159,7 +161,16 @@ void SystemClock_Config(void)
 }
 
 /* USER CODE BEGIN 4 */
-
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+ if(htim == &htim1)
+ {
+   a++;
+   HAL_GPIO_TogglePin(RGB_B_GPIO_Port, RGB_B_Pin);
+   HAL_GPIO_TogglePin(RGB_G_GPIO_Port, RGB_G_Pin);
+   HAL_GPIO_TogglePin(RGB_R_GPIO_Port, RGB_R_Pin);
+ }   
+}
 /* USER CODE END 4 */
 
 /**
--- zero_program/MDK-ARM/zero_program/tim.d
+++ zero_program/MDK-ARM/zero_program/tim.d
@@ -1,6 +1,5 @@
-zero_program/stm32f4xx_hal_tim.o: \
-  ..\Drivers\STM32F4xx_HAL_Driver\Src\stm32f4xx_hal_tim.c \
-  ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal.h \
+zero_program/tim.o: ..\Core\Src\tim.c ..\Core\Inc\tim.h \
+  ..\Core\Inc\main.h ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal.h \
   ..\Core\Inc\stm32f4xx_hal_conf.h \
   ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_rcc.h \
   ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_def.h \
